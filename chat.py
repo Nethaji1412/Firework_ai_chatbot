5de760c85fa63e457981484be9295f3baafb579c
Nethaji Ram <nethajitvm150@gmail.com>
2026-05-25 18:45:38 +0530

    Update chat.py

--- chat.py
+++ chat.py
@@ -74,7 +74,7 @@ if "messages" not in st.session_state:
         {"role": "assistant", "content": """
 You are JARVIS, a highly intelligent assistant.
 Respond concisely, formally, assistively and firendly.
-Be precise, avoid unnecessary text, and prioritize usefulness.
+Be precise, avoid unnecessary text, and prioritize usefulness.you must be respond in tamil
 """}
     ]
 
